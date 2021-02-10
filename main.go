54cafc8dbca4131c360b586a01681440e2bcfd74
Kevin Mencos <mecosk@gmail.com>
2021-02-09 20:53:06 -0600

    Add morse imp

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	var interpreter translator.Service
 	interpreter = translator.Interpreter{}
 	fmt.Println(interpreter.Translate("AAAA","TEXT", "BINARY"))
+	fmt.Println(interpreter.Translate("AAAA","TEXT", "MORSE"))
 }
--- translator/morse.go
+++ translator/morse.go
@@ -1,6 +1,10 @@
 package translator
 
+import "fmt"
+
 type Morse struct{}
+
+
 
 
 func (Morse) TranslateToText(text string) string {
@@ -8,6 +12,30 @@ func (Morse) TranslateToText(text string) string {
 }
 
 func (Morse) TranslateToTarget(text string) string {
-	return ""
+	var translation string
+	for _, c := range text {
+		translation = fmt.Sprintf("%s%s", translation, textToMorse(string(c)))
+	}
+	return translation
 }
 
+func textToMorse(text string) string{
+	morseTable := map[string]string {
+		"A": ".-",
+		"B": "-...",
+		"C": "-.-.",
+		"D": "-..",
+		"E": ".",
+		"F": "..-.",
+		"G": "--.",
+		"H": "....",
+		"I": "..",
+		"J": ".---",
+		"K": "-.-",
+		"L": ".-..",
+		"M": "--",
+		"N": "-.",
+		"O": "---",
+	}
+	return morseTable[text]
+}
